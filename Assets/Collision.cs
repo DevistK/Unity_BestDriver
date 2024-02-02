4195467a5bde102fa7ac3f7bfa362eb50e625bd9
DevistK <donghyeok.dev@gmail.com>
2024-02-02 16:49:59 +0900

    feat : CollisionEnter 충돌 이벤트 추가

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter2D(Collision2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // OnCollisionEnter 내장 메서드를 사용해서 Rigid Body와 Collider 가 적용된 물리 객체에 충돌 이벤트를 만들어줄 수 있다.
+        // 2D 는 스프라이트 전용 , 2D 가 안붙어 있으면 3D 전용
+        Debug.Log($"Crush !!");
     }
 }
